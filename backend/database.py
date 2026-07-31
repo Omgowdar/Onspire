e9b995257da7d288bbdca5920acc1e6c2357e2a0
Omgowdar <ggowdar.cs24@bmsce.ac.in>
2026-07-31 11:57:48 +0530

    Initial project commit

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from typing import Generator
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./gigshield.db"
 
